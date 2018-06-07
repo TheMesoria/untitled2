cd1ae0a2dca9dac23ba979f913eca564aef634a2
Black <freedomandjustit@gmail.com>
2018-06-07 19:58:52 +0200

    save

---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from distutils.core import setup
+
+setup(name='untitled2',
+      version='1.0',
+      author='Ormaniec',
+      author_email='226181@student.pwr.edu.pl',
+      url='https://github.com/TheMesoria/untitled2',
+      packages=['untitled2', 'untitled2.utils'],
+      )
---
+++ utils/ormaniec_add.py
@@ -0,0 +1,4 @@
+
+def ormaniec_add(val):
+    assert isinstance(val, int)
+    return 20 + val;
